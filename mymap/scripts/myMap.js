6f6b2cf9dcb204ac6bc56217e7b5e40a68d508d3
quilkin <quilkin2@gmail.com>
2015-05-02 13:32:36 +0100

    map test 2

--- mymap/scripts/myMap.js
+++ mymap/scripts/myMap.js
@@ -4,12 +4,32 @@
     var myMap = {};
 
     myMap.create = function () {
-        var map = L.map('map').setView([50.505, -5], 13);
+        // var map = L.map('map').setView([50.505, -5], 13);
+        var map = L.map('map');
+
 
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
             maxZoom: 18
         }).addTo(map);
+        map.on('locationerror', onLocationError);
+        map.on('locationfound', onLocationFound);
+        map.locate({ setView: true, maxZoom: 16 });
     }
+
+    function onLocationFound(e) {
+        var radius = e.accuracy / 2;
+
+        L.marker(e.latlng).addTo(map)
+            .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+        L.circle(e.latlng, radius).addTo(map);
+    }
+    function onLocationError(e) {
+        alert(e.message);
+    }
+
+
+
     return myMap
 })(jQuery)
