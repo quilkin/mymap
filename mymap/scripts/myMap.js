86c545f8474150af6ba16ea2f39ddccf94136b78
quilkin <quilkin2@gmail.com>
2015-05-02 13:14:05 +0100

    sync test

---
+++ mymap/scripts/myMap.js
@@ -0,0 +1,15 @@
+﻿var myMap = (function ($) {
+    "use strict";
+
+    var myMap = {};
+
+    myMap.create = function () {
+        var map = L.map('map').setView([50.505, -5], 13);
+
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+            maxZoom: 18
+        }).addTo(map);
+    }
+    return myMap
+})(jQuery)
